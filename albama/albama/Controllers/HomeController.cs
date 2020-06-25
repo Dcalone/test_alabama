5cf20c6f51c8f00154d7c8768a12b9af554bf9e7
taoye0122 <taoye0122@qq.com>
2020-06-26 07:59:57 +0800

    frist day

---
+++ albama/albama/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using albama.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace albama.Controllers
+{
+    public class HomeController:Controller
+    {
+        public HomeController(IClock clock)
+        {
+
+        }
+    }
+}
--- albama/albama/Startup.cs
+++ albama/albama/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using albama.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +17,18 @@ namespace albama
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // MVC用 够用了 
+            services.AddControllersWithViews();
+            // 过于强大 不必要
+            //services.AddMvc();
+            // 做接口使用
+            //services.AddControllers();
+            //生命周期 服务创建到结束
+            services.AddSingleton<IClock, ChinaClock>();
+            //每次新建服务都会创建一个新的实例
+            //services.AddScoped
+            //每次请求都回新建一个
+            //services.AddTransient
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
