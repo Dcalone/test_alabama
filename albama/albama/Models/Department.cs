0678ec714b5ca3851bff29c7520aec0a708bac24
taoye0122 <taoye0122@qq.com>
2020-06-27 14:39:20 +0800

    创建表单是视图

--- albama/albama/Models/Department.cs
+++ albama/albama/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace albama.Models
     public class Department
     {
         public int Id { get; set; }
+        [Display(Name = "部门名称")]
         public string Name { get; set; }
         public string Location { get; set; }
         public int EmployeeCount { get; set; }
